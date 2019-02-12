a3a43410a840f94ac14a9e5083ab608661e0a99a
erikmarto <erikmarto12@gmail.com>
2019-02-12 09:09:50 +0100

    api nueva

---
+++ Yii/restApi/models/Actividades.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+class Actividades extends \yii\db\ActiveRecord
+{
+    /* public function fields(){
+            return ['id','actividades'];
+        } */
+}
+?>
